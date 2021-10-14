6cf53f7beaadd540ac2df67b19f40903d0f4582c
Ksenx73 <hello@itcampe.com>
2021-10-14 16:50:50 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,12 @@
+package it.campe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String emptiness = "пустота";
+
+        String fullness = ""; //напишите тут ваш код
+
+        System.out.println(fullness);
+    }
+}
